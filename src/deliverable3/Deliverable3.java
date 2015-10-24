2246d74880b390859a576363495e1a9ac593f944
jda34 <jda34@pitt.edu>
2015-10-23 21:15:17 -0400

    First commit

---
+++ src/deliverable3/Deliverable3.java
@@ -0,0 +1,20 @@
+package deliverable3;
+
+import static org.junit.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.junit.Test;
+
+public class Deliverable3 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	
+	@Test
+	public void testMe(){
+		WebDriver driver = new FirefoxDriver();
+	}
+}
